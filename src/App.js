f0b3e526717d02f1eb04ce64be0be7a1a6a5e8fb
mokdanbi <l26tempo@naver.com>
2022-09-29 18:33:19 +0900

    0929

--- src/App.js
+++ src/App.js
@@ -11,13 +11,24 @@ import Community from './pages/Community';
 import Mealncook from './pages/Mealncook';
 import './css/App.scss'
 
+
 function App() {
+  const NAVTITLE = [
+    { id: 0, title: "브랜드", link: "/brand" },
+    { id: 1, title: "메뉴", link: "/menu" },
+    { id: 2, title: "매장안내", link: "/store" },
+    { id: 3, title: "창업문의", link: "/franchise" },
+    { id: 4, title: "공지사항", link: "/notice" },
+    { id: 5, title: "커뮤니티", link: "/community" },
+    { id: 6, title: "밀앤쿡", link: "/mealncook" }
+  ]
+
   return (
     <Wrapper>
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/brand" element={<Brand />} />
+        <Route path="/brand" element={<Brand content={NAVTITLE} />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/store" element={<Store />} />
         <Route path="/franchise" element={<Franchise />} />
--- src/pages/Brand.js
+++ src/pages/Brand.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../css/Subpage.scss'
+import { brand_sub } from '../component/NavData';
+import { Link } from 'react-router-dom';
 
-const Brand = () => {
+const Brand = ({ content }) => {
+  const num = 0;
   return (
     <div className='Subpage'>
       <div className="subImg"></div>
+      <div className="navBar">
+        <div className="inner">
+          <div className="title">
+            {content[num].title}
+          </div>
+          <ul className="list">
+            <li style={{fontWeight:700}}><Link to="/brand">{brand_sub[0].title}</Link></li>
+            <li><Link to="/brand/sub02">{brand_sub[1].title}</Link></li>
+            <li><Link to="/brand/sub03">{brand_sub[2].title}</Link></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
